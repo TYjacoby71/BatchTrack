5a8f12c82a6c3c0abca68340610f7747ab54427e
Jacob Boulette <39427985-jacoby72@users.noreply.replit.com>
2025-04-11 00:26:48 +0000

    Restored to 'bd6216a948b022ce57c56ca49e111ec02320961d'
    Replit-Restored-To: bd6216a948b022ce57c56ca49e111ec02320961d

--- models.py
+++ models.py
@@ -41,7 +41,6 @@ class Batch(db.Model):
     notes = db.Column(db.Text)
     tags = db.Column(db.Text)
     total_cost = db.Column(db.Float)
-    status = db.Column(db.String(32), default='in_progress')
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
