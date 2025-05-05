4608d6d8ce2c0813e19241be062d7ef4764576b6
Jacob Boulette <39427985-jacoby72@users.noreply.replit.com>
2025-05-05 17:18:01 +0000

    Restored to 'b4bb50c5780ab18ef8b2b27e1cf4243db8a78734'
    Replit-Restored-To: b4bb50c5780ab18ef8b2b27e1cf4243db8a78734

--- static/js/products/product_log.js
+++
@@ -1,8 +0,0 @@
-
-// Product log functionality
-function viewProductLog(productId) {
-  window.location.href = `/products/${productId}/log`;
-}
-
-// Make functions globally available
-window.viewProductLog = viewProductLog;
