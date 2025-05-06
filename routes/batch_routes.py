404c4f2c3c7a7bdb3214700fd1b65aa8bce5c237
Jacob Boulette <39427985-jacoby72@users.noreply.replit.com>
2025-05-06 22:27:37 +0000

    Restored to 'd4b79df1c3e193bb291a9fca9f23c0dbe8086c8e'
    Replit-Restored-To: d4b79df1c3e193bb291a9fca9f23c0dbe8086c8e

--- routes/batch_routes.py
+++ routes/batch_routes.py
@@ -209,17 +209,14 @@ def view_batch_in_progress(batch_identifier):
     # Only pass batch_cost if ingredients are used
     
 
-    inventory_items = InventoryItem.query.filter_by(type='ingredient').all()
+    inventory_items = InventoryItem.query.all()
     return render_template('batch_in_progress.html',
                          batch=batch,
                          recipe=recipe,
                          units=units,
                          batch_cost=batch_cost,
                          product_quantity=product_quantity,       
-                         inventory_items=inventory_items,
-                         ingredients=batch.ingredients,
-                         containers=batch.containers,
-                         timers=batch.timers)
+                         inventory_items=inventory_items)
 
 @batches_bp.route('/<int:batch_id>/finish', methods=['POST'])
 @login_required
