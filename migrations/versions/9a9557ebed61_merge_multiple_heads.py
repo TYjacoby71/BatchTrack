fe0d4cb6f1594e9e2e4e82e74d125e4fa81528c3
Jacob Boulette <jacobboulette@outlook.com>
2025-07-30 21:47:20 +0000

    Restored to '2a8011bd1f9abdd330e094539d5b4528accd2066'
    Replit-Restored-To: 2a8011bd1f9abdd330e094539d5b4528accd2066

--- migrations/versions/9a9557ebed61_merge_multiple_heads.py
+++ migrations/versions/9a9557ebed61_merge_multiple_heads.py
@@ -1,8 +1,7 @@
-
 """merge multiple heads
 
 Revision ID: 9a9557ebed61
-Revises: f8e9d7c6b5a4, 6f7401b4d435
+Revises: 6f7401b4d435, d4b5c89f1234, f8e9d7c6b5a4
 Create Date: 2025-07-30 19:25:40.657996
 
 """
@@ -12,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '9a9557ebed61'
-down_revision = ('f8e9d7c6b5a4', '6f7401b4d435')
+down_revision = ('f8e9d7c6b5a4', '6f7401b4d435', 'd4b5c89f1234')
 branch_labels = None
 depends_on = None
 
