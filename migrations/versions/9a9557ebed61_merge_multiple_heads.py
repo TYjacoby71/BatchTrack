a218ac308efeb529501a37aa57f54f721b17739e
Jacob Boulette <jacobboulette@outlook.com>
2025-07-31 01:38:44 +0000

    revert to working db

---
+++ migrations/versions/9a9557ebed61_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""merge multiple heads
+
+Revision ID: 9a9557ebed61
+Revises: requires_stripe_billing
+Create Date: 2025-07-30 19:25:40.657996
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9a9557ebed61'
+down_revision = ('73e07c67ddb0', '6f7401b4d435')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
