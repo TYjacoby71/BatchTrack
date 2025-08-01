e6e6d0d4f1eb7461b003b081e18a7f86b1804545
Jacob Boulette <jacobboulette@outlook.com>
2025-08-01 23:40:10 +0000

    Restored to 'cb7ce7edf731cf41a004680b6ca15a5537a2cb9b'
    Replit-Restored-To: cb7ce7edf731cf41a004680b6ca15a5537a2cb9b

--- migrations/versions/fix_user_role_assignment_constraints.py
+++ migrations/versions/fix_user_role_assignment_constraints.py
@@ -1,7 +1,7 @@
 
 """fix user_role_assignment constraints to allow NULL role_id for developer roles
 
-Revision ID: fix_user_role_constraints
+Revision ID: fix_user_role_assignment_constraints
 Revises: fix_password_hash_length
 Create Date: 2025-02-01 03:00:00.000000
 
