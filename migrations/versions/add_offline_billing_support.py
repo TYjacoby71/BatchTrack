81d211a9bc1fe67288664d19987ac5fc4f4745ee
Jacob Boulette <jacobboulette@outlook.com>
2025-08-06 06:52:15 +0000

    Restored to '7b3af2201f4ac102e0061e354ae60ebd7a308fe8'
    Replit-Restored-To: 7b3af2201f4ac102e0061e354ae60ebd7a308fe8

--- migrations/versions/add_offline_billing_support.py
+++ migrations/versions/add_offline_billing_support.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers
 revision = 'add_offline_billing_support'
-down_revision = '8b7aa70df87d'
+down_revision = 'drop_deprecated_billing_columns'
 branch_labels = None
 depends_on = None
 
